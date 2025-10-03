6bc8cd3a0937ebc6d103181e471450ef112bd652
Wangska <wangska1283@gmail.com>
2025-10-04 03:49:47 +0800

    changes

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration using environment variables
-$host = $_ENV['DB_HOST'] ?? 'joccwsg44gwo0oc0s8g48ko0';
+$host = $_ENV['DB_HOST'] ?? 'scsok4o44go4owco408ggocc';
 $dbname = $_ENV['DB_DATABASE'] ?? 'default';
 $username = $_ENV['DB_USERNAME'] ?? 'root';
-$password = $_ENV['DB_PASSWORD'] ?? '0439VpH3ybiZZdDmbFH7mI1PXBBSSGfMwh96BdMEcBGGYH5gJsIFZAiTAfCwNJGz';
+$password = $_ENV['DB_PASSWORD'] ?? 'p4j7AYzUO0bFC8sAgdGgYb2wKkzZzkGPYfy2Dk9hxUZKDSC1eEVDCVmjZklPjSGI';
 $port = $_ENV['DB_PORT'] ?? '3306';
 
 try {
